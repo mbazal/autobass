ca64319f79e39ae9cbb7b0ab173a6a4b8d4dd0a6
mbazal <mohammadbazal@gmail.com>
2025-09-26 11:21:58 -0400

    added argument validation function

--- archive.sh
+++ archive.sh
@@ -4,3 +4,10 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo " Makes target_dir/backup_YYYYMMDD_HHMMSS and puts the copy of contents there."
     exit 0
 fi
+
+# check number of arguments 
+if [ $# -ne 2 ]; then
+    echo "Incorrect number of arguments. Use -h or --help for usage."
+    exit 1
+fi
+
