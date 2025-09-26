9e24c89c2880911ad2c0b7e83987e89cad6b4134
mbazal <mohammadbazal@gmail.com>
2025-09-26 11:24:44 -0400

    added directory validation function

--- archive.sh
+++ archive.sh
@@ -11,3 +11,17 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+# assign arguments to variables and validate directories
+SRC="$1"
+DST="$2"
+
+if [ ! -d "$SRC" ]; then
+  echo "Error: The source directory was not found: $SRC" >&2
+  echo "Use -h or --help for usage."
+  exit 1
+fi
+if [ ! -d "$DST" ]; then
+  echo "Error: The target directory was not found: $DST" >&2
+  echo "Use -h or --help for usage."
+  exit 1
+fi
